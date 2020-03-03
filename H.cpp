46e0b2ee7141a786aa308b47667f01a219ecd007
MDASIKURRAHMAN <mdasikur1.25@gmail.com>
2020-03-03 11:53:53 +0600

    hh

---
+++ H.cpp
@@ -0,0 +1,18 @@
+/*
+ * H.cpp
+ *
+ *  Created on: Feb 2, 2020
+ *      Author: pc
+ */
+
+#include "H.h"
+
+H::H() {
+	// TODO Auto-generated constructor stub
+
+}
+
+H::~H() {
+	// TODO Auto-generated destructor stub
+}
+
